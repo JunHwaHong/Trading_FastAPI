c588ceec1a72f12ab824d1401102b080fae5759f
JH <44192730+JunHwaHong@users.noreply.github.com>
2023-03-14 17:40:38 +0900

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
-DB_FILE = 'C:\\Users\\hong9\\Jupyter_Projects\\quantlib\\ai_in_finance\\Trading_App\\app.db'
+DB_FILE = '...\\app.db'
 API_URL = 'https://paper-api.alpaca.markets'
-API_KEY = 'PKI228VJLXNCT7YPL3WU'
-SECRET_KEY = 'cWDqUo2vT9XWqiApUNCg4IZtrxsdzjywOF9DbP8G'
+API_KEY = '...'
+SECRET_KEY = '...'
 
 EMAIL_ADDRESS = 'email'
-EMAIL_PASSWORD = 'pass'
+EMAIL_PASSWORD = 'password'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 465
